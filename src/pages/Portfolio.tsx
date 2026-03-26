ff84c7c3cc3c00ae26cd006e5c719c22c909ca87
luiz armendro <luizfelipearmendro>
2026-03-25 22:01:42 -0400

    retirado a categoria de Retratos do portfólio

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -3,7 +3,7 @@ import PageHero from "@/components/PageHero";
 import FadeIn from "@/components/FadeIn";
 import { useState } from "react";
 
-const categories = ["Todos", "Retratos", "Ensaios", "Fotografias Corporativas"];
+const categories = ["Todos", "Ensaios", "Fotografias Corporativas"];
 
 const photos = [
   { src: "/images/ensaios/FullSizeRender 2-20092025.jpg", cat: "Ensaios" },
@@ -30,22 +30,6 @@ const photos = [
   { src: "/images/ensaios/IMG_7825-20092025.jpg", cat: "Ensaios" },
   { src: "/images/ensaios/IMG_3780 - Copia.jpg", cat: "Ensaios" },
   { src: "/images/ensaios/IMG_7827-20092025.jpg", cat: "Ensaios" },
-  { src: "/images/retratos/25bb859a-6b12-4ea4-bf22-c856cd8d1d17.jpg", cat: "Retratos" },
-  { src: "/images/retratos/64e66bb8-c8d9-4a91-ba04-6d6e2208991e.JPG", cat: "Retratos" },
-  { src: "/images/retratos/598d6689-731f-4a89-9e2c-8a052a148627.jpg", cat: "Retratos" },
-  { src: "/images/retratos/935b157b-82a1-45d1-86d1-7f4c92264a43.JPG", cat: "Retratos" },
-  { src: "/images/retratos/3321dcae-6e62-4ed0-aa29-c8577c21593c.JPG", cat: "Retratos" },
-  { src: "/images/retratos/56334648-1117-4558-a9a3-7e288615c70e.JPG", cat: "Retratos" },
-  { src: "/images/retratos/abf06386-3419-49c8-9dcc-0dd66ecb72bf.JPG", cat: "Retratos" },
-  { src: "/images/retratos/c3fcfbfa-d414-4ca3-b0bc-ae30c5006c81.JPG", cat: "Retratos" },
-  { src: "/images/retratos/c6163bde-7736-4689-987d-0828f68724fe.JPG", cat: "Retratos" },
-  { src: "/images/retratos/cpm35 2025-01-23 124122.714.JPG", cat: "Retratos" },
-  { src: "/images/retratos/cpm35 2025-01-25 111311.122.JPG", cat: "Retratos" },
-  { src: "/images/retratos/cpm35 2025-01-28 073600.843.JPG", cat: "Retratos" },
-  { src: "/images/retratos/cpm35 2025-01-28 073849.273.JPG", cat: "Retratos" },
-  { src: "/images/retratos/cpm35 2025-01-28 131425.520.JPG", cat: "Retratos" },
-  { src: "/images/retratos/cpm35 2025-01-28 131425.992.JPG", cat: "Retratos" },
-  { src: "/images/retratos/cpm35 2025-01-28 131426.916.JPG", cat: "Retratos" },
 ];
 
 const PortfolioPage = () => {
